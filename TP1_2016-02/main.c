d08ee8ab500a4fc5b7602e562899db6e837aa970
Gabriel S. Pires <gabrielspires@live.com>
2016-04-14 01:40:29 -0300

    TP Final

--- TP1_2016-02/main.c
+++ TP1_2016-02/main.c
@@ -1,6 +1,8 @@
 #include "funcoes.h"
 
 int main(int argc, char const *argv[]){
+	int i;
+	FILE *saida;
 	Ponto matchPoint;
 	PGM *cena, *obj; //Estrutura do tipo PGM que guarda os dados lidos do arquivo de imagem
 	cena = LePGM((char*)argv[1]); //A estrutura 'imagem' recebe os dados lidos na função
@@ -8,7 +10,18 @@ int main(int argc, char const *argv[]){
 
 	matchPoint = JanelaDeslizante(cena, obj);
 
-	printf("%d %d\n", matchPoint.y, matchPoint.x);
+	saida = fopen(argv[3], "w");
+	fprintf(saida, "%d %d\n", matchPoint.y, matchPoint.x);
+
+	fclose(saida);
+	
+	for(i=0; i<cena->l; i++) free(cena->dados[i]);
+	free(cena->dados);
+	free(cena);
+	
+	for(i=0; i<obj->l; i++) free(obj->dados[i]);
+	free(obj->dados);
+	free(obj);
 
 	return 0;
 }
